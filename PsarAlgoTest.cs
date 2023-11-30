b14eba1bb80a18782b38c725c125d66775806396
Rod C <cayabyabrr@yahoo.com>
2023-11-29 18:12:57 -0800

    compiles

--- PsarAlgoTest.cs
+++ PsarAlgoTest.cs
@@ -27,12 +27,16 @@ namespace NinjaTrader.NinjaScript.Strategies
 {
     public class PsarAlgoTest : Strategy
     {
-        private double upperChannelOffset = 0.1;  // Adjust this value based on your requirements
-        private double supportOffset = 0.1;       // Adjust this value based on your requirements
+        // private double upperChannelOffset = 0.1;  // Adjust this value based on your requirements
+        // private double supportOffset = 0.1;       // Adjust this value based on your requirements
 
         private double psar;
-        private double upperChannel;
-        private double support;
+        // private double upperChannel;
+        // private double support;
+
+        private double trend;
+
+        private Indicator TrendLines;
 
         private Indicator parabolicSar;
 
@@ -45,27 +49,52 @@ namespace NinjaTrader.NinjaScript.Strategies
             else if (State == State.Configure)
             {
                 AddDataSeries(Data.BarsPeriodType.Tick, 1); // Assuming tick data for MNQ
+                // PSARPARAMS = [0.02, 0.2, 0.02]
                 parabolicSar = ParabolicSAR(0.02, 0.2, 0.02);
             }
             else if (State == State.DataLoaded) {
-                upperChannel = Highs[1][0] + upperChannelOffset;
-                lowerChannel = Lows[1][0] - supportOffset;
-                
+                // upperChannel = Highs[1][0] + upperChannelOffset;
+                // lowerChannel = Lows[1][0] - supportOffset;
+            
+
+                // upperChannel.Plots[0].Brush = Brushes.Goldenrod;
+                // lowerChannel.Plots[0].Brush = Brushes.SeaGreen;
+
+                // AddChartIndicator(upperChannel);
+                // AddChartIndicator(lowerChannel);
+
+                AddPlot(Brushes.Green, "parabolicSAR"); // stored as Plots[0] and Values[0]
+                // AddPlot(Brushes.Blue, "lowerChannel"); // stored as Plots[1] and Values[1]
+
+                // Values[0][0] = ParabolicSAR(parabolicSar)[0];
+                Values[0][0] = ParabolicSAR(0.02, 0.2, 0.02)[0];
+
+                // Values[1][0] = trend(lowerChannel)[0];
+
+                // TrendLines(int strength, int numberOfTrendLines, int oldTrendsOpacity, bool alertOnBreak)
+                // return val double
+
+//                trend = TrendLines(5, 1, 3, false);
+                AddPlot(Brushes.Blue, "trendLines");
+                Values[1][0] = TrendLines(5, 1, 3, false)[0];
+
             }
         }
 
         protected override void OnBarUpdate()
         {
-                psar = parabolicSar[0];
-                upperChannel = Highs[1][0] + upperChannelOffset;
-                lowerChannel = Lows[1][0] - supportOffset;
 
-            if (CurrentBar < BarsRequiredToTrade)
+            if (CurrentBar < BarsRequiredToTrade) {
                 return;
-
-            if (IsRising(psar)) {
-                EnterLong();
             }
+
+                // psar = parabolicSar[0];
+
+               // bool for above and below to trigger trades
+
+            // if (IsRising(trend)) {
+                EnterLong();
+            // }
         }
     }
 }
