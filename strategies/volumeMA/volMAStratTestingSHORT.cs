55b3d531a918cd424c9f8e69f441072b53240223
Scruffy pc <cayabyabrr@yahoo.com>
2024-02-08 21:19:17 -0800

    volMAextended scripts

---
+++ strategies/volumeMA/volMAStratTestingSHORT.cs
@@ -0,0 +1,153 @@
+#region Using declarations
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Xml.Serialization;
+using NinjaTrader.Cbi;
+using NinjaTrader.Gui;
+using NinjaTrader.Gui.Chart;
+using NinjaTrader.Gui.SuperDom;
+using NinjaTrader.Gui.Tools;
+using NinjaTrader.Data;
+using NinjaTrader.NinjaScript;
+using NinjaTrader.Core.FloatingPoint;
+using NinjaTrader.NinjaScript.Indicators;
+using NinjaTrader.NinjaScript.DrawingTools;
+#endregion
+
+//This namespace holds Strategies in this folder and is required. Do not change it. 
+namespace NinjaTrader.NinjaScript.Strategies
+{
+	public class volMAStratTestingSHORT : Strategy
+	{
+		protected override void OnStateChange()
+		{
+			if (State == State.SetDefaults)
+			{
+				Description									= @"Enter the description for your new custom Strategy here.";
+				Name										= "volMAStratTestingSHORT";
+				// Calculate									= Calculate.OnBarClose;
+				Calculate									= Calculate.OnEachTick;
+				EntriesPerDirection							= 1;
+				EntryHandling								= EntryHandling.AllEntries;
+				IsExitOnSessionCloseStrategy				= true;
+				ExitOnSessionCloseSeconds					= 30;
+				IsFillLimitOnTouch							= false;
+				MaximumBarsLookBack							= MaximumBarsLookBack.TwoHundredFiftySix;
+				OrderFillResolution							= OrderFillResolution.Standard;
+				Slippage									= 0;
+				StartBehavior								= StartBehavior.WaitUntilFlat;
+				TimeInForce									= TimeInForce.Gtc;
+				TraceOrders									= false;
+				RealtimeErrorHandling						= RealtimeErrorHandling.StopCancelClose;
+				StopTargetHandling							= StopTargetHandling.PerEntryExecution;
+				BarsRequiredToTrade							= 20;
+				// Disable this property for performance gains in Strategy Analyzer optimizations
+				// See the Help Guide for additional information
+				IsInstantiatedOnEachOptimizationIteration	= true;
+
+				Vol_UD_barsAgo = 0;
+				tick_size = 2;
+				stopLoss_tick_size = 10;
+				profitTarget_tick_size = 20;
+				valueMultiplier = 2;
+
+				SMA_med = 50;
+
+				AddPlot(Brushes.DarkViolet, "SMA_med");
+
+
+			}
+			else if (State == State.Configure)
+			{
+			}
+		}
+
+		// market order lags behind 1 bar if not tick replay
+		protected override void OnBarUpdate()
+		{
+			if (BarsInProgress != 0 || CurrentBars[0] < 3) 
+				return;
+
+			double Vol_UD_Val = VolumeUpDown()[0];
+			bool firstBar_isGreen = Close[1] >= Open[1];
+			bool secondBar_isGreen = Close[2] >= Open[2];
+			bool thirdBar_isRed = Close[3] < Open[3];
+
+			double first = VOL()[1];
+			double second = VOL()[2];
+
+			double SMA_medVal = SMA(SMA_med)[0];
+
+
+			// if previous two bars are each RED & at least twice the size of 3rd GREEN bar
+			// if (Close[0] > Vol_UD_Val) 
+			if ((thirdBar_isRed && secondBar_isGreen && firstBar_isGreen) 
+				&& (VOL()[1] > VOL()[3]*valueMultiplier)
+				&& (VOL()[2] > VOL()[3]*valueMultiplier)
+				&& Close[0] < SMA_medVal
+				) 
+				{
+					Print("The current Volume value is " + Vol_UD_Val.ToString());
+					Print("firstBar_isRed : " + first.ToString());
+					Print("secondBar_isRed : " + second.ToString());
+					Print("thirdBar_isGreen: ++" + VolumeUpDown()[3].ToString());
+					Print("thirdBar_isGreen:VOLVOL ++" + VOL()[3].ToString());
+					Print("---------------");
+
+				EnterShort("Enter Short");
+				SetStopLoss(CalculationMode.Ticks, stopLoss_tick_size);
+        		SetProfitTarget(CalculationMode.Ticks, profitTarget_tick_size);
+				}
+
+
+				
+		}
+		#region Properties
+		[NinjaScriptProperty]
+		[Range(0, int.MaxValue)]
+		[Display(Name="Vol_UD_barsAgo", Order=1, GroupName="Parameters")]
+		public int Vol_UD_barsAgo
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Range(0, double.MaxValue)]
+		[Display(Name="tick_size", Order=2, GroupName="Parameters")]
+		public double tick_size
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Range(0, double.MaxValue)]
+		[Display(Name="stopLoss_tick_size", Order=3, GroupName="Parameters")]
+		public double stopLoss_tick_size
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Range(0, double.MaxValue)]
+		[Display(Name="profitTarget_tick_size", Order=4, GroupName="Parameters")]
+		public double profitTarget_tick_size
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Range(1, double.MaxValue)]
+		[Display(Name="valueMultiplier", Order=5, GroupName="Parameters")]
+		public double valueMultiplier
+		{ get; set; }
+
+		// SMA indicator
+		[NinjaScriptProperty]
+		[Range(10, int.MaxValue)]
+		[Display(Name="SMA_med", Order=6, GroupName="Parameters")]
+		public int SMA_med
+		{ get; set; }
+		#endregion
+
+	}
+}
---
+++ strategies/volumeMAextended/volMAStratExtendedLiveLag.cs
@@ -0,0 +1,183 @@
+#region Using declarations
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Xml.Serialization;
+using NinjaTrader.Cbi;
+using NinjaTrader.Gui;
+using NinjaTrader.Gui.Chart;
+using NinjaTrader.Gui.SuperDom;
+using NinjaTrader.Gui.Tools;
+using NinjaTrader.Data;
+using NinjaTrader.NinjaScript;
+using NinjaTrader.Core.FloatingPoint;
+using NinjaTrader.NinjaScript.Indicators;
+using NinjaTrader.NinjaScript.DrawingTools;
+#endregion
+
+//This namespace holds Strategies in this folder and is required. Do not change it. 
+namespace NinjaTrader.NinjaScript.Strategies
+{
+	public class volMAStratExtendedLiveLag : Strategy
+	{
+		protected override void OnStateChange()
+		{
+			if (State == State.SetDefaults)
+			{
+				Description									= @"Enter the description for your new custom Strategy here.";
+				Name										= "volMAStratExtendedLiveLag";
+				// Calculate									= Calculate.OnBarClose;
+				Calculate									= Calculate.OnEachTick;
+				EntriesPerDirection							= 1;
+				EntryHandling								= EntryHandling.AllEntries;
+				IsExitOnSessionCloseStrategy				= true;
+				ExitOnSessionCloseSeconds					= 30;
+				IsFillLimitOnTouch							= false;
+				MaximumBarsLookBack							= MaximumBarsLookBack.TwoHundredFiftySix;
+				OrderFillResolution							= OrderFillResolution.Standard;
+				Slippage									= 0;
+				StartBehavior								= StartBehavior.WaitUntilFlat;
+				TimeInForce									= TimeInForce.Gtc;
+				TraceOrders									= false;
+				RealtimeErrorHandling						= RealtimeErrorHandling.StopCancelClose;
+				StopTargetHandling							= StopTargetHandling.PerEntryExecution;
+				BarsRequiredToTrade							= 20;
+				// Disable this property for performance gains in Strategy Analyzer optimizations
+				// See the Help Guide for additional information
+				IsInstantiatedOnEachOptimizationIteration	= true;
+
+				Vol_UD_barsAgo = 0;
+				tick_size = 2;
+				stopLoss_tick_size = 18;
+				profitTarget_tick_size = 23;
+				valueMultiplier = 2.1;
+
+				SMA_med = 50;
+
+				AddPlot(Brushes.DarkViolet, "SMA_med");
+			}
+			else if (State == State.Configure)
+			{
+			}
+		}
+
+		protected override void OnBarUpdate()
+		// protected override void OnMarketData(MarketDataEventArgs marketDataUpdate)
+		{
+			// if (BarsInProgress != 0 || CurrentBars[0] < 3 || !IsFirstTickOfBar) 
+			if (BarsInProgress != 0 || CurrentBars[0] < 4)  {
+				Print("Waiting for more than 3 bars");
+				return;
+			}
+
+			double Vol_UD_Val = VolumeUpDown()[0];
+			bool secondBar_isRed = Close[2] < Open[2];
+			bool thirdBar_isRed = Close[3] < Open[3];
+			bool fourthBar_isGreen = Close[4] >= Open[4];
+
+			double secondRED = VOL()[2];
+			double thirdRED = VOL()[3];
+			double fourthGREEN = VOL()[4];
+
+			double fourthBarVOL_mult = VOL()[4] * valueMultiplier;
+
+			double SMA_medVal = SMA(SMA_med)[0];
+			Values[0][0] = SMA_medVal;
+
+			// if 2nd and 3rd bar back is RED & 4th bar is green, if each 2nd and 3rd bar's vol times valueMultiplier's value are more than 4th bar's vol & is above SMA_medVal
+			if ((fourthBar_isGreen && thirdBar_isRed && secondBar_isRed) 
+				&& (VOL()[2] > VOL()[4]*valueMultiplier)
+				&& (VOL()[3] > VOL()[4]*valueMultiplier)
+				&& (Close[0] > SMA_medVal)
+				) 
+				{
+
+					Print("The current Volume value is " + Vol_UD_Val.ToString());
+					Print("2ndbarRED : " + secondRED.ToString());
+					Print("3rdbarRED : " + thirdRED.ToString());
+					Print("4thBarGREEN: ++" + fourthGREEN.ToString());
+					Print("ValMultiplier: " + (VOL()[4]*valueMultiplier).ToString());
+					Print("---------------");
+
+				EnterLong("Enter Long");
+				SetStopLoss(CalculationMode.Ticks, stopLoss_tick_size);
+        		SetProfitTarget(CalculationMode.Ticks, profitTarget_tick_size);
+				} else if ((fourthBar_isGreen && thirdBar_isRed && secondBar_isRed) && Close[0] > SMA_medVal) {
+					Print("2nd & 3rd RED, 4th Green & Close / SMA" + Close[0].ToString() + " / " + SMA_medVal.ToString() );
+				}
+
+		// 	// Sets SL/PT when limit order hits
+		// 	protected override void OnOrderUpdate(Order order, double limitPrice, double stopPrice, int quantity, int filled, double averageFillPrice, OrderState orderState, DateTime time, ErrorCode error, string nativeError)
+		// {
+		// 	if (order.Name == "PSAR entry" && orderState != OrderState.Filled)
+		// 	{
+		// 		entryOrder = order;
+		// 		SetStopLoss(CalculationMode.Ticks, stopLoss_tick_size);
+		// 		SetProfitTarget(CalculationMode.Ticks, profitTarget_tick_size);
+
+		// 	}
+
+		// 	if (entryOrder != null && entryOrder == order)
+		// 	{
+		// 		if (order.OrderState == OrderState.Cancelled && order.Filled == 0)
+		// 		{
+		// 			entryOrder = null;
+		// 		}
+		// 		if (order.OrderState == OrderState.Filled)
+		// 		{
+		// 			entryOrder = null;
+		// 		}
+		// 	}
+		// }
+
+
+
+	}
+		#region Properties
+		[NinjaScriptProperty]
+		[Range(0, int.MaxValue)]
+		[Display(Name="Vol_UD_barsAgo", Order=1, GroupName="Parameters")]
+		public int Vol_UD_barsAgo
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Range(0, double.MaxValue)]
+		[Display(Name="tick_size", Order=2, GroupName="Parameters")]
+		public double tick_size
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Range(0, double.MaxValue)]
+		[Display(Name="stopLoss_tick_size", Order=3, GroupName="Parameters")]
+		public double stopLoss_tick_size
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Range(0, double.MaxValue)]
+		[Display(Name="profitTarget_tick_size", Order=4, GroupName="Parameters")]
+		public double profitTarget_tick_size
+		{ get; set; }
+
+		[NinjaScriptProperty]
+		[Range(1, double.MaxValue)]
+		[Display(Name="valueMultiplier", Order=5, GroupName="Parameters")]
+		public double valueMultiplier
+		{ get; set; }
+
+		// SMA indicator
+		[NinjaScriptProperty]
+		[Range(10, int.MaxValue)]
+		[Display(Name="SMA_med", Order=6, GroupName="Parameters")]
+		public int SMA_med
+		{ get; set; }
+		#endregion
+
+	}
+}
